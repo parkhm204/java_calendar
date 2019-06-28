17481ce31e540cb284e66dd18e11642e0518182f
jlee <jlee@xevo.com>
2019-06-28 22:25:49 +0900

    what is a month of lastDay

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package honux.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -9,6 +11,16 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 
+		//숫자를 입력 받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("달을 입력하세요");
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; //int[] 배열타입이라는 의미 
+	
+		System.out.printf("%d월은 %d일까지 있습니다. \n", month, maxDays[month - 1]);
+		
+		scanner.close();
 	}
 
 }
