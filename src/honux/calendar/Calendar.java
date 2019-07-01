d2dc39fb7cab2a07f188f53bcb77921cb5769c2e
jlee <jlee@xevo.com>
2019-07-01 18:39:33 +0900

    view a month of calendar

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,26 +1,38 @@
 package honux.calendar;
 
-import java.util.Scanner;
+
 
 public class Calendar {
-
-	public static void main(String[] args) {
-		System.out.println(" 일 월  화 수  목  금 토");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-
-		//숫자를 입력 받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		Scanner scanner = new Scanner (System.in);
-		System.out.println("달을 입력하세요");
-		int month = scanner.nextInt();
-		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; //int[] 배열타입이라는 의미 
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; //int[] 배열타입이라는 의미 
 	
-		System.out.printf("%d월은 %d일까지 있습니다. \n", month, maxDays[month - 1]);
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+	
+
+	public void printCalendar(int year, int month)  {
+		System.out.printf("   <<%4d년%3d월>>\n",year, month);
 		
-		scanner.close();
+		System.out.println("SU MO TU WE TH FR SA");
+		System.out.println("--------------------");
+		
+		int maxDay = getMaxDaysOfMonth(month);
+		
+		for(int i = 1; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if(i % 7 == 0)
+				System.out.println();
+		}
+		
+		System.out.println();
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
+
+
+		
+	
 	}
 
 }
